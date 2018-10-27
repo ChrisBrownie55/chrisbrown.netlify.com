5ba5f1e5583b3e47157983af8e973efafa0fa874
Christopher Brown <chbphone55@gmail.com>
2018-10-27 10:00:44 -0600

    Added smooth-scroll wrapper

--- src/smooth-scroll.js
+++ src/smooth-scroll.js
@@ -1,1 +1,32 @@
-import smoothscroll from 'smoothscroll'
+import { PolymerElement, html } from '@polymer/polymer'
+import ScrollModule from '../node_modules/smooth-scroll-module/src/scroll-module'
+
+const scroller = new ScrollModule()
+
+class SmoothScroll extends PolymerElement {
+  constructor() {
+    super()
+    this.addEventListener('click', this.smoothScroll.bind(this))
+  }
+
+  smoothScroll() {
+    const element = document.querySelector(this.getAttribute('target'))
+    if (!element) { return }
+
+    scroller.scrollTo(element)
+  }
+
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: contents;
+        }
+      </style>
+
+      <slot></slot>
+    `
+  }
+}
+
+window.customElements.define('smooth-scroll', SmoothScroll)
---
+++ src/theme-button.js
@@ -0,0 +1,76 @@
+import { PolymerElement, html } from '@polymer/polymer'
+
+class ThemeButton extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          position: relative;
+
+          padding: 0.85rem 1.75rem;
+
+          background: none;
+          color: var(--light-text);
+
+          border: solid 2px var(--theme-primary);
+          border-radius: 50px;
+
+          font-size: 1rem;
+          font-weight: 700;
+          font-family: Montserrat, var(--default-fonts);
+          text-transform: uppercase;
+
+          transition: padding 0.2s, color 0.2s, background-color 0.2s;
+          will-change: padding, color, background-color;
+          cursor: pointer;
+        }
+
+        :host(::after) {
+          content: '';
+
+          position: absolute;
+          top: calc(100% + 4px);
+          left: 50%;
+
+          width: 0;
+          height: 2px;
+          border-radius: 14px;
+
+          background-color: rgba(254, 215, 102, 0.8);
+
+          transform: translateX(-50%);
+          transition: width 0.2s;
+        }
+
+        :host(:active) {
+          padding: 0.85rem 2.75rem;
+
+          color: var(--dark-text);
+          background-color: var(--theme-primary);
+
+          -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+        }
+
+        @media (hover: hover) {
+          :host(:hover) {
+            padding: 0.85rem 2.75rem;
+
+            color: var(--dark-text);
+            background-color: var(--theme-primary);
+          }
+        }
+
+        :host(:focus) {
+          outline: none;
+        }
+
+        :host(:focus:not(:active)::after) {
+          width: 80%;
+        }
+      </style>
+      <slot></slot>
+    `
+  }
+}
+
+window.customElements.define('theme-button', ThemeButton)
