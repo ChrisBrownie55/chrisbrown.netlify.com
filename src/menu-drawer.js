72a7c41ee4651974413974295e458d959f133311
Christopher Brown <chbphone55@gmail.com>
2018-10-31 17:42:05 -0600

    Use updated instead of render

--- src/menu-drawer.js
+++ src/menu-drawer.js
@@ -69,13 +69,18 @@ class MenuDrawer extends LitElement {
     }
   }
 
+  updated(changedProperties) {
+    super.updated(changedProperties)
+    if (changedProperties.has('open')) {
+      document.body.style.overflow = this.open === true ? 'hidden' : ''
+    }
+  }
+
   toggle() {
     this.open = !this.open
   }
 
   render() {
-    document.body.style.overflow = this.open === true ? 'hidden' : ''
-
     return html`
       <input type='checkbox' id='menu__checkbox'
         @change=${e => this.open = e.target.checked}
