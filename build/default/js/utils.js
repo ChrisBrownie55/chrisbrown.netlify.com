60989e9045556f70a6affba58a841a4457a3133a
Christopher Brown <chbphone55@gmail.com>
2018-10-31 21:09:17 -0600

    remove build stuff

--- build/default/js/utils.js
+++
@@ -1,3 +0,0 @@
-// brightness is on a scale from 0 -> 255
-export const brightnessFromRGB = (r, g, b) => (r * 299 + g * 587 + b * 114) / 1000;
-export const brightnessFromElementBackground = el => brightnessFromRGB(...getComputedStyle(el).backgroundColor.slice(4, -1).split(',').map(x => parseInt(x)));
