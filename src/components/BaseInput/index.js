d561d62ab0574b5253e27ad3fa5b13b1f6665364
Christopher Brown <chbphone55@gmail.com>
2018-11-14 01:31:00 -0700

    Working story for base-input

--- src/components/BaseInput/index.js
+++ src/components/BaseInput/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 let counter = 0;
 
-const BaseInput = ({ label, ...props }) => {
+const BaseInput = ({ label, ...props }) => (
+  <div className="base-input">
+    {label ? <label htmlFor={"base-input-"+counter}>{label}</label> : null}
+    <input id={"base-input-"+(counter++)} {...props} />
+  </div>
+);
 
-  return (
-    <div className="base-input">
-      {label ? <label htmlFor={"base-input-"+counter}>{label}</label> : null}
-      <input id={"base-input-"+(counter++)} {...props} />
-    </div>
-  )
-};
+BaseInput.propTypes = {
+  label: PropTypes.string.isRequired
+}
 
 export default BaseInput;
--- stories/index.js
+++ stories/index.js
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { checkA11y } from '@storybook/addon-a11y';
 
 import BaseInput from '../src/components/BaseInput';
 
 storiesOf('BaseInput', module)
-  .add('with label', () => (
+  .addDecorator(checkA11y)
+  .add('basic setup', () => (
     <BaseInput label="Name" name="name"></BaseInput>
   ))
   .add('with placeholder', () => (
-    <BaseInput placeholder="Chris Brown" name="name"></BaseInput>
-  ))
-  .add('with label and placeholder', () => (
     <BaseInput label="Name" placeholder="Chris Brown" name="name"></BaseInput>
   ))
-  .add('with bound value', function() {
-    const [value, setValue] = useState('');
-    function handleChange(event) {
-      action('text entered')(event)
-      setValue(event.target.value)
-    }
+  .add('with bound value', () => (
+    <ButtonWithHooks></ButtonWithHooks>
+  ))
 
-    return (
-      <BaseInput value={value} onChange={handleChange}></BaseInput>
-    )
-  })
+function ButtonWithHooks() {
+  const [value, setValue] = useState('');
+  function handleChange(event) {
+    action('text changed')(event);
+    setValue(event.target.value);
+  }
+
+  return [
+    <BaseInput key={0} label="Name" name="name" value={value} onChange={handleChange}></BaseInput>,
+    <p key={1}>Value: "{value}"</p>
+  ]
+}
