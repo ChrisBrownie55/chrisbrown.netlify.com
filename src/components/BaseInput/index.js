35a804234ce156e474c764c49b267b6d0bafa204
Christopher Brown <chbphone55@gmail.com>
2018-11-16 12:03:44 -0700

    semicolons

--- src/components/BaseInput/index.js
+++ src/components/BaseInput/index.js
@@ -8,26 +8,26 @@ let counter = 0;
 const BaseInput = ({ label, theme, ...props }) => {
   const onChange = event => {
     if (event.target.value) {
-      event.target.classList.add('notempty')
+      event.target.classList.add('notempty');
     } else {
-      event.target.classList.remove('notempty')
+      event.target.classList.remove('notempty');
     }
 
     if (props.onChange) {
-      props.onChange(event)
+      props.onChange(event);
     }
-  }
+  };
 
   return (
     <div className="base-input" theme={theme}>
       <input id={"base-input-"+(counter)} {...props} onChange={onChange} />
       <label htmlFor={"base-input-"+counter++}>{label}</label>
     </div>
-  )
+  );
 };
 
 BaseInput.propTypes = {
   label: PropTypes.string.isRequired
-}
+};
 
 export default BaseInput;
--- src/components/ThemeButton/index.js
+++ src/components/ThemeButton/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import './index.css';
 
-export default props => <button className="theme-button" {...props}></button>
+export default props => <button className="theme-button" {...props}></button>;
