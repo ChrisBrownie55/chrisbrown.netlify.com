0f59cfc3c049967a0245b74abe75b159e128de4d
Christopher Brown <chbphone55@gmail.com>
2018-11-16 12:03:29 -0700

    import css

--- src/components/FlatButton/index.js
+++ src/components/FlatButton/index.js
@@ -1,3 +1,4 @@
 import React from 'react';
+import './index.css';
 
-export default props => <button className="flat-button" {...props}></button>
+export default props => <button className="flat-button" {...props}></button>;
