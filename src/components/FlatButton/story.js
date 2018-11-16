15bfc305dd1f9d104151ed1395da2b482fd226ff
Christopher Brown <chbphone55@gmail.com>
2018-11-16 12:01:24 -0700

    flat-button story

---
+++ src/components/FlatButton/story.js
@@ -0,0 +1,13 @@
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import centered from '@storybook/addon-centered';
+
+import React, { useContext } from 'react';
+import FlatButton from './';
+import ThemeContext from '../../contexts/theme.js';
+
+storiesOf('ThemeButon', module)
+  .addDecorator(centered)
+  .addWithJSX('basic setup', () =>(
+    <FlatButton onClick={action('clicked')}>Lorem Ipsum</FlatButton>
+  ));
