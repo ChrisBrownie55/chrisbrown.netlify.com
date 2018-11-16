3fba8c15135d55c449e357c1aa48cc58cb73a0d6
Christopher Brown <chbphone55@gmail.com>
2018-11-15 18:21:57 -0700

    Fixed counter for base-input

--- src/components/BaseInput/story.js
+++ src/components/BaseInput/story.js
@@ -1,28 +1,29 @@
-import { storiesOf } from '@storybook/react';
+import { storiesOf, setAddon } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import React, { useState, useContext } from 'react';
 import BaseInput from './';
 import ThemeContext from '../../contexts/theme.js';
 
+
 storiesOf('BaseInput', module)
-  .add('basic setup', () => (
+  .addWithJSX('basic setup', () => (
     <BaseInput label="Name" name="name"></BaseInput>
   ))
-  .add('with placeholder', () => (
+  .addWithJSX('with placeholder', () => (
     <BaseInput label="Name" placeholder="Chris Brown" name="name"></BaseInput>
   ))
-  .add('with bound value', () => (
+  .addWithJSX('with bound value', () => (
     <InputWithHooks></InputWithHooks>
   ))
-  .add('with dark theme', () => (
+  .addWithJSX('with dark theme', () => (
     <ThemeContext.Provider value="dark">
       <div style={{backgroundColor: "#222", padding: "2rem"}}>
         <ThemedInput></ThemedInput>
       </div>
     </ThemeContext.Provider>
   ))
-  .add('with theme color', () => (
+  .addWithJSX('with theme color', () => (
     <div style={{"--theme-primary": "#fed766"}}>
       <BaseInput label="Name" name="name"></BaseInput>
     </div>
