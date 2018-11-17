53e5a8ab586c7080eb1f27c9be60f5ccf202791a
Christopher Brown <chbphone55@gmail.com>
2018-11-16 23:34:35 -0700

    remove themeContext

--- src/components/ThemeButton/story.js
+++ src/components/ThemeButton/story.js
@@ -4,7 +4,6 @@ import centered from '@storybook/addon-centered';
 
 import React, { useContext } from 'react';
 import ThemeButton from './';
-import ThemeContext from '../../contexts/theme.js';
 
 storiesOf('ThemeButon', module)
   .addDecorator(centered)
@@ -12,23 +11,12 @@ storiesOf('ThemeButon', module)
     <ThemeButton onClick={action('clicked')}>Lorem Ipsum Dolor Sit</ThemeButton>
   ))
   .add('with dark theme', () => (
-    <ThemeContext.Provider value="dark">
-      <div style={{backgroundColor: "#222", padding: "2rem"}}>
-        <ThemedThemeButton>
-        </ThemedThemeButton>
-      </div>
-    </ThemeContext.Provider>
+    <div style={{backgroundColor: "#222", padding: "2rem"}}>
+      <ThemeButton theme="dark">Lorem Ipsum Dolor Sit</ThemeButton>
+    </div>
   ))
   .add('with theme color', () => (
     <div style={{backgroundColor: "#222", padding: "2rem"}}>
       <ThemeButton style={{"--theme-primary": "#fed766"}} theme="dark">Lorem Ipsum Dolor Sit</ThemeButton>
     </div>
   ));
-
-
-function ThemedThemeButton() {
-  const theme = useContext(ThemeContext);
-  return (
-    <ThemeButton theme={theme}>Lorem Ipsum Dolor Sit</ThemeButton>
-  );
-}
