9305dfbe0310aecdaca58752d554789b6f4d841c
Christopher Brown <chbphone55@gmail.com>
2018-11-17 14:46:12 -0700

    with theme color

--- src/components/SocialMediaLink/story.js
+++ src/components/SocialMediaLink/story.js
@@ -32,3 +32,19 @@ storiesOf('SocialMediaLink', module)
       </SocialMediaLink>
     </div>
   ))
+  .add('with theme color', () => (
+    <WithFigma url='https://www.figma.com/file/g7O9K8Y1BVY3TJPSOdTU4EHA/Footer'>
+      <div style={{backgroundColor: "#66C9FE", padding: "2rem"}}>
+        <SocialMediaLink
+          href="#"
+          title="Check out my Facebook"
+          theme="dark"
+          style={{"--theme-secondary": "#66C9FE"}}
+        >
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='-9 -1 54 74'>
+            <path d='M36 0v14.4h-7.2c-2.484 0-3.6 2.916-3.6 5.4v9H36v14.4H25.2V72H10.8V43.2H0V28.8h10.8V14.4C10.8 6.447 17.247 0 25.2 0H36' />
+          </svg>
+        </SocialMediaLink>
+      </div>
+    </WithFigma>
+  ))
