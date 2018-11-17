deaf0ede15162938a3b9e44f77198e2780b62494
Christopher Brown <chbphone55@gmail.com>
2018-11-17 13:37:19 -0700

    socialmediabutton

--- src/components/SocialMediaButton/index.js
+++ src/components/SocialMediaButton/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import './index.css';
 
 const SocialMediaButton = props => <a {...props}></a>
+
 SocialMediaButton.propTypes = {
   href: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired
--- src/components/SocialMediaButton/story.js
+++ src/components/SocialMediaButton/story.js
@@ -9,8 +9,9 @@ storiesOf('SocialMediaButton', module)
   .add('basic setup', () => (
     <WithFigma url='https://www.figma.com/file/g7O9K8Y1BVY3TJPSOdTU4EHA/Footer'>
       <SocialMediaButton
-        href='https://www.facebook.com/chbchb55'
+        href='#'
         title='Check out my Facebook'
+        onClick={action('clicked')}
       >
         <svg xmlns='http://www.w3.org/2000/svg' viewBox='-9 -1 54 74'>
           <path d='M36 0v14.4h-7.2c-2.484 0-3.6 2.916-3.6 5.4v9H36v14.4H25.2V72H10.8V43.2H0V28.8h10.8V14.4C10.8 6.447 17.247 0 25.2 0H36' />
