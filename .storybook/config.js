05c3d832e5570caf65f7cad2f043862f069e8ec0
Christopher Brown <chbphone55@gmail.com>
2018-11-16 23:19:42 -0700

    center all stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure, addDecorator } from '@storybook/react';
 
 import { checkA11y } from '@storybook/addon-a11y';
 import { withNotes } from '@storybook/addon-notes';
+import centered from '@storybook/addon-centered';
 
 import { setConfig } from 'react-hot-loader';
 
@@ -9,6 +10,7 @@ setConfig({ pureSFC: true });
 
 addDecorator(checkA11y);
 addDecorator(withNotes);
+addDecorator(centered);
 
 const req = require.context('../src/components/', true, /story\.js$/);
 
