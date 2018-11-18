ea93cfcf9423dc51e24c1b8ac2052bd14fd21b40
Christopher Brown <chbphone55@gmail.com>
2018-11-17 22:01:16 -0700

    allow scrolling in story

--- src/components/SmoothScroll/story.js
+++ src/components/SmoothScroll/story.js
@@ -5,13 +5,19 @@ import React from 'react';
 import SmoothScroll from './';
 
 storiesOf('SmoothScroll', module)
-  .add('basic setup', () => (
-    <div>
-      <SmoothScroll target="#the-target">
-        <button>CLICK TO SCROLL</button>
-      </SmoothScroll>
-      <div id="the-target" style={{marginTop: "calc(100vh + 4rem)"}}>
-        <h1>I should have been smoothly scrolled into view!</h1>
+  .add('basic setup', () => {
+    requestIdleCallback(() => {
+      document.getElementById('root').children[0].style.position = '';
+    });
+
+    return (
+      <div>
+        <SmoothScroll target="#the-target">
+          <button>CLICK TO SCROLL</button>
+        </SmoothScroll>
+        <div id="the-target" tabIndex="0" style={{marginTop: "calc(100vh + 4rem)"}}>
+          <h1>I should have been smoothly scrolled into view!</h1>
+        </div>
       </div>
-    </div>
-  ));
+    )
+  });
