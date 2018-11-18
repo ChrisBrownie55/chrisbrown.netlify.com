26e1338953577f94278e0e95d75ed39f3a8906bb
Christopher Brown <chbphone55@gmail.com>
2018-11-17 18:16:41 -0700

    smooth-scroll story

---
+++ src/components/SmoothScroll/story.js
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import React from 'react';
+import SocialMediaLink from './';
+
+storiesOf('SmoothScroll', module);
