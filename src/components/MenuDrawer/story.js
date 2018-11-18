74198c033fef8ff436fe700ee3a9b47636908f53
Christopher Brown <chbphone55@gmail.com>
2018-11-18 16:05:38 -0700

    setup

---
+++ src/components/MenuDrawer/story.js
@@ -0,0 +1,8 @@
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { WithFigma } from 'storybook-addon-figma';
+
+import React from 'react';
+import MenuDrawer from './';
+
+storiesOf('MenuDrawer', module);
