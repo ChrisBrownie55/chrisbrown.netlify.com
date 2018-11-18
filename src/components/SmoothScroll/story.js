6779ed351e1fa2f5a1197541b19925543784e124
Christopher Brown <chbphone55@gmail.com>
2018-11-17 18:26:31 -0700

    basic story

--- src/components/SmoothScroll/story.js
+++ src/components/SmoothScroll/story.js
@@ -2,6 +2,16 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 
 import React from 'react';
-import SocialMediaLink from './';
+import SmoothScroll from './';
 
-storiesOf('SmoothScroll', module);
+storiesOf('SmoothScroll', module)
+  .add('basic setup', () => (
+    <div>
+      <SmoothScroll target="#the-target">
+        <button>CLICK TO SCROLL</button>
+      </SmoothScroll>
+      <div id="the-target" style={{marginTop: "calc(100vh + 4rem)"}}>
+        <h1>I should have been smoothly scrolled into view!</h1>
+      </div>
+    </div>
+  ));
