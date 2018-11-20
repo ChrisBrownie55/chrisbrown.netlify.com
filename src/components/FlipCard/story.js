bf7b504f36743b9050b5ae2cf6b80558f443c496
Christopher Brown <chbphone55@gmail.com>
2018-11-20 11:32:47 -0700

    Story boilerplate

---
+++ src/components/FlipCard/story.js
@@ -0,0 +1,7 @@
+import { storiesOf } from '@storybook/react';
+import { WithFigma } from 'storybook-addon-figma';
+
+import React from 'react';
+import FlipCard from './';
+
+storiesOf('FlipCard', module);
