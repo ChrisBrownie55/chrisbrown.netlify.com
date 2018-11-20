0cd28e1f352978570db84b544a91fcd4c5babc2f
Christopher Brown <chbphone55@gmail.com>
2018-11-19 20:10:39 -0700

    state

--- src/components/FlipCard/index.js
+++ src/components/FlipCard/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FlipCard = ({ years, label }) => {
+  const { 0: isFlipped, 1: setFlipped } = useState(false);
+  function toggleFlipped() {
+    setFlipped(!isFlipped);
+  }
 
+  function handleKeyUp(event) {
+    // if key is a space
+    if (event.key === ' ') {
+      toggleFlipped();
+    }
+  }
+  function handleKeyDown(event) {
+    if (event.key === ' ') {
+      event.preventDefault();
+    }
+  }
+
+  return (
+    
+  );
 };
 
 FlipCard.propTypes = {
