db2305e590f9b5ec3f5d7a1bcc1792d4b72cfe37
Christopher Brown <chbphone55@gmail.com>
2018-11-20 14:28:16 -0700

    import css & remove flickity.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Hero from './views/Hero';
 
+import './style/main.css';
+import './style/system-font.css';
+
 const App = () => {
   return (
     <main className="app-container">
--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
-import './style/main.css';
-import './style/system-font.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
