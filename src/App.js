c88eb7d467ee947efbf45188d08432a814836f36
Christopher Brown <chbphone55@gmail.com>
2018-11-20 14:24:18 -0700

    display Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Hero from './views/Hero';
 
 const App = () => {
   return (
     <main className="app-container">
+      <Hero></Hero>
     </main>
   );
-}
+};
 
 export default App;
