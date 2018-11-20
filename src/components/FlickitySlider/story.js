b16d383356aa43707b6662ca05bc7db6365f4116
Christopher Brown <chbphone55@gmail.com>
2018-11-20 13:04:00 -0700

    fixed flickity-slider

--- src/components/FlickitySlider/story.js
+++ src/components/FlickitySlider/story.js
@@ -1,7 +1,29 @@
 import { storiesOf } from '@storybook/react';
-import { WithFigma } from 'storybook-addon-figma';
+// import { WithFigma } from 'storybook-addon-figma';
 
 import React from 'react';
 import FlickitySlider from './';
 
-storiesOf('FlickitySlider', module);
+storiesOf('FlickitySlider', module)
+  .add('basic setup', () => [
+    <style>
+    {`.flickity-enabled {
+      width: 20rem;
+      height: 20rem;
+    }
+    img {
+      width: 20rem;
+      height: 20rem;
+      object-fit: cover;
+    }`}
+    </style>,
+    <FlickitySlider>
+      <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="placeholder" />
+      <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="placeholder" />
+      <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="placeholder" />
+      <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="placeholder" />
+      <img src="https://images.pexels.com/photos/1563355/pexels-photo-1563355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="placeholder" />
+    </FlickitySlider>
+  ], {
+    notes: 'Images inside of the FlickitySlider component must have a height.'
+  });
