399ad1da8d72f366317122878b100d28d1ac0287
Christopher Brown <chbphone55@gmail.com>
2018-11-20 10:55:39 -0700

    to jsx

--- src/components/FlipCard/index.js
+++ src/components/FlipCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FlipCard = ({ years, label }) => {
+const FlipCard = ({ years, label, ...props }) => {
   const { 0: isFlipped, 1: setFlipped } = useState(false);
   function toggleFlipped() {
     setFlipped(!isFlipped);
@@ -20,7 +20,28 @@ const FlipCard = ({ years, label }) => {
   }
 
   return (
-    
+    <div>
+      <figure class="content" role="switch"
+        tabindex="0"
+        onClick={this.toggle}
+        onKeyup={this._handleKeyUp}
+        onKeydown={this._handleKeyDown}
+        flipped={this.flipped}
+        aria-checked={!!this.flipped}>
+        {props.children}
+        <figcaption className="content-back">
+          <p>
+            <span className="years">
+              ${this.years}
+            </span>
+            <sup>yrs</sup>
+          </p>
+        </figcaption>
+      </figure>
+      <p className="name">
+        ${this.name}
+      </p>
+    </div>
   );
 };
 
