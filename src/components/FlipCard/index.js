4325a7c6b8ce999413bf6fcca63e0bfe8d541702
Christopher Brown <chbphone55@gmail.com>
2018-11-19 20:10:32 -0700

    basic

---
+++ src/components/FlipCard/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FlipCard = ({ years, label }) => {
+
+};
+
+FlipCard.propTypes = {
+  years: PropTypes.number.isRequired,
+  label: PropTypes.string.isRequired
+};
+
+export default FlipCard;
