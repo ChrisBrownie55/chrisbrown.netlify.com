f650e2e50417e80dc4ec532a7f6cd3dcd07d5e08
Christopher Brown <chbphone55@gmail.com>
2018-11-20 20:00:41 -0700

    use native smooth scroll

--- src/components/SmoothScroll/index.js
+++ src/components/SmoothScroll/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import ScrollModule from 'smooth-scroll-module/src/scroll-module';
 import { traverseDownUntil } from '../../utils.js';
 
-const scroller = new ScrollModule();
 const prefersReducedMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
 
 const SmoothScroll = ({ target, children: element }) => {
@@ -15,45 +13,13 @@ const SmoothScroll = ({ target, children: element }) => {
         ? target.current
         : document.querySelector(target);
     if (!element) { return; }
-
-    await scroller.scrollTo(element, prefersReducedMotion);
-
-    const focusableElement = traverseDownUntil(node => {
-      if (node.disabled) {
-        return false;
-      }
-
-      const tabIndex = parseInt(node.getAttribute('tabindex'));
-      if (tabIndex < 0) {
-        return false;
-      } else if (!isNaN(tabIndex)) {
-        return true;
-      }
-
-      switch (node.tagName) {
-        case 'INPUT':
-        case 'BUTTON':
-        case 'IFRAME':
-          return true;
-        case 'A':
-        case 'AREA':
-          return !!node.getAttribute('href');
-        default:
-          return false;
-      }
-    }, element);
-
-    if (focusableElement) {
-      focusableElement.focus();
-    } else {
-      console.warn('Could not find a focusable element, found this instead:', focusableElement);
-    }
+    element.scrollIntoView({ behavior: prefersReducedMotion ? 'instant' : 'smooth' })
   }
 
   const elementWithListener = React.cloneElement(
     element,
     { onClick: onClick.bind(this) }
-  )
+  );
 
   return elementWithListener;
 };
