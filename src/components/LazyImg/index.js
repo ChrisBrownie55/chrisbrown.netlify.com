8fe3fbb201f60ea73c7ca793ad45e0191de9cf0b
Christopher Brown <chbphone55@gmail.com>
2018-11-21 20:35:03 -0700

    LazyImg

---
+++ src/components/LazyImg/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(({ intersectionRatio, target }) => {
+    if (intersectionRatio >= 0.05) {
+      observer.unobserve(target);
+      target.setAttribute('src', 'data-src');
+      target.removeAttribute('data-src');
+    }
+  })
+})
+const LazyImg = ({ src, alt, ...props }) => {
+  return (
+    <img
+      src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+      data-src={src}
+      alt={alt}
+      {...props}
+    />
+  );
+};
+
+LazyImg.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+};
+
+export default React.memo(LazyImg);
