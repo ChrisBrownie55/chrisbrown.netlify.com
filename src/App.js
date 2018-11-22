579429083de7452d1bed3a8c3054b96a6c3d14a5
Christopher Brown <chbphone55@gmail.com>
2018-11-21 20:35:32 -0700

    Lazy load about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Hero from './views/Hero';
 
 import './style/main.css';
 import './style/system-font.css';
+
+import Hero from './views/Hero';
+const About = React.lazy(() => import('./views/About'));
 
 const App = () => {
   return (
     <main className="app-container">
       <Hero></Hero>
+      <React.Suspense fallback={() => <p>Loading...</p>}>
+        <About></About>
+      </React.Suspense>
     </main>
   );
 };
