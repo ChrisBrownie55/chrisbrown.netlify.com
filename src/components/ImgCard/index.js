c340063baec4c5025b3298734461d86702f0ffac
Christopher Brown <chbphone55@gmail.com>
2018-11-22 13:36:15 -0700

    whoo!

--- src/components/ImgCard/index.js
+++ src/components/ImgCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import LazyImg from '../LazyImg';
 import { join } from '../../utils';
 
 import './index.css';
@@ -8,11 +9,11 @@ import './index.css';
 const ImgCard = ({ src, alt, href, children, className, ...props}) => {
   return (
     <div className={join("ImgCard", className)} {...props}>
-      <img className="ImgCard-image" src={src} alt={alt} />
+      <LazyImg target=".mywork" className="ImgCard-image" src={src} alt={alt} />
       <div className="ImgCard-content">
-        <p className="ImgCard-description">
+        <div className="ImgCard-description">
           {children}
-        </p>
+        </div>
         <a className="ImgCard-explore-link" href={href}>Explore</a>
       </div>
     </div>
@@ -20,7 +21,7 @@ const ImgCard = ({ src, alt, href, children, className, ...props}) => {
 }
 
 ImgCard.propTypes = {
-  src: PropTypes.string.isRequired,
+  src: PropTypes.func.isRequired,
   alt: PropTypes.string.isRequired,
   href: PropTypes.string.isRequired
 }
--- src/views/MyWork.js
+++ src/views/MyWork.js
@@ -8,7 +8,7 @@ const MyWork = () => (
 <h1 className="mywork__title title">My Work</h1>
 <FlickitySlider>
   <section className="slide">
-    <ImgCard src="assets/inspire-min.png" alt="Inspire" href="https://chrisbrownie55.github.io/inspire/">
+    <ImgCard src={() => import("../assets/inspire-min.png")} alt="Inspire" href="https://chrisbrownie55.github.io/inspire/">
       <strong>Inspire</strong> is a highly functional and customizeable
       homepage with numerous features:
       <ul>
@@ -29,7 +29,7 @@ const MyWork = () => (
         </li>
       </ul>
     </ImgCard>
-    <ImgCard src="assets/vue-music-min.png" alt="Vue Music" href="https://vusic.herokuapp.com">
+    <ImgCard src={() => import("../assets/vue-music-min.png")} alt="Vue Music" href="https://vusic.herokuapp.com">
       <strong>Vue Music</strong> is a Spotify clone using the iTunes
       API. Here is a list of its features:
       <ul>
@@ -47,7 +47,7 @@ const MyWork = () => (
         </li>
       </ul>
     </ImgCard>
-    <ImgCard src="assets/partyox-min.png" alt="Party Ox" href="https://partyox.herokuapp.com">
+    <ImgCard src={() => import("../assets/partyox-min.png")} alt="Party Ox" href="https://partyox.herokuapp.com">
       <strong>Party Ox</strong> is a PWA that allows a DJ to receive,
       accept, and reject suggestions from members of their parties.
       <ul>
@@ -67,7 +67,7 @@ const MyWork = () => (
     </ImgCard>
   </section>
   <section className="slide">
-    <ImgCard src="assets/stanager-min.png" alt="Stanager" href="https://www.npmjs.com/package/stanager">
+    <ImgCard src={() => import("../assets/stanager-min.png")} alt="Stanager" href="https://www.npmjs.com/package/stanager">
       <strong>Stanager</strong> is an npm module that provides an
       immutable state manager. Here are its features:
       <ul>
@@ -86,7 +86,7 @@ const MyWork = () => (
         </li>
       </ul>
     </ImgCard>
-    <ImgCard src="assets/battle-cards-min.png" alt="Battle Cards" href="https://chrisbrownie55.github.io/battle-cards">
+    <ImgCard src={() => import("../assets/battle-cards-min.png")} alt="Battle Cards" href="https://chrisbrownie55.github.io/battle-cards">
       <strong>Battle Cards</strong> is a card fighting game in which the
       player selects one of their cards and one of the enemies to fight.
       <br />
@@ -95,7 +95,7 @@ const MyWork = () => (
       transitions when cards fight. Along with this is the use of Axios
       to interact with the game&lsquo;s API.
     </ImgCard>
-    <ImgCard src="assets/keepr-min.png" alt="Keepr" href="https://keepr1.herokuapp.com">
+    <ImgCard src={() => import("../assets/keepr-min.png")} alt="Keepr" href="https://keepr1.herokuapp.com">
       <strong>Keepr</strong> is a Full Stack clone of Pinterest. It was
       built using the following technologies:
       <ul>
@@ -114,16 +114,16 @@ const MyWork = () => (
     </ImgCard>
   </section>
   <section className="slide">
-    <ImgCard src="assets/iacs-counselors-min.png" alt="IACS Counselors" href="https://iacs-counselor.github.io/">
+    <ImgCard src={() => import("../assets/iacs-counselors-min.png")} alt="IACS Counselors" href="https://iacs-counselor.github.io/">
       <strong>IACS Counselors</strong> is a static mobile-friendly
       website I designed, content-edited, and developed for the
       counselor&lsquo;s of IACS.
     </ImgCard>
-    <ImgCard src="assets/how-to-make-your-code-readable-min.png" alt="How to Make Your Code Readable" href="https://medium.com/s/story/the-importance-of-readable-code-165895e939c7">
+    <ImgCard src={() => import("../assets/how-to-make-your-code-readable-min.png")} alt="How to Make Your Code Readable" href="https://medium.com/s/story/the-importance-of-readable-code-165895e939c7">
       <strong>How to Make Your Code Readable</strong> is an article that I
       wrote on the importance of writing <i>&quot;clean code&quot;</i>. It was featured on Medium.com and has been read over ten thousand times.
     </ImgCard>
-    <ImgCard src="assets/franciscan-faculty-min.png" alt="Franciscan React" href="https://github.com/fus-marcom/franciscan-react/pull/146">
+    <ImgCard src={() => import("../assets/franciscan-faculty-min.png")} alt="Franciscan React" href="https://github.com/fus-marcom/franciscan-react/pull/146">
       I helped design and build several of the pages for
       <strong style={{whiteSpace: "nowrap"}}>
         Franciscan University&lsquo;s
