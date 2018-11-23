6dedf0902698fd323ceb1d175eb154b0d809bd02
Christopher Brown <chbphone55@gmail.com>
2018-11-23 15:01:31 -0700

    Add spinner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './style/main.css';
 import './style/system-font.css';
 
 import Hero from './views/Hero';
+
+import MDSpinner from "react-md-spinner";
 const About = React.lazy(() => import('./views/About'));
 const Expertise = React.lazy(() => import('./views/Expertise'));
 const ChallengeSolution = React.lazy(() => import('./views/ChallengeSolution'));
@@ -11,26 +13,28 @@ const MyWork = React.lazy(() => import('./views/MyWork'));
 const Hire = React.lazy(() => import('./views/Hire'));
 const Contact = React.lazy(() => import('./views/Contact'));
 
+const Spinner = <MDSpinner style={{margin: "calc(20vh - 40px) calc(50vw - 40px)"}} size={80} singleColor="var(--theme-secondary)" />;
+
 const App = () => {
   return (
     <main className="app-container">
       <Hero></Hero>
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={Spinner}>
         <About></About>
       </React.Suspense>
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={Spinner}>
         <Expertise></Expertise>
       </React.Suspense>
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={Spinner}>
         <ChallengeSolution></ChallengeSolution>
       </React.Suspense>
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={Spinner}>
         <MyWork></MyWork>
       </React.Suspense>
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={Spinner}>
         <Hire></Hire>
       </React.Suspense>
-      <React.Suspense fallback={<p>Loading...</p>}>
+      <React.Suspense fallback={Spinner}>
         <Contact></Contact>
       </React.Suspense>
     </main>
