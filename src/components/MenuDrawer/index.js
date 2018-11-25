2038c7510d4f0ea4c6c3c47f775cd5974e090645
Christopher Brown <chbphone55@gmail.com>
2018-11-25 11:14:28 -0700

    remove unused var

--- src/components/MenuDrawer/index.js
+++ src/components/MenuDrawer/index.js
@@ -14,7 +14,6 @@ const MenuDrawer = ({ children, sections }) => {
     height: 0
   };
 
-  let sectionElements = []
   const colorMap = new Map();
 
   const { 0: isOpen, 1: setOpen } = useState(false);
