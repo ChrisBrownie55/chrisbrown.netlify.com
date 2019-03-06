9390582fbe001e206f78cb3fe87c551906cbdf92
Christopher Brown <chbphone55@gmail.com>
2019-03-05 22:24:45 -0700

    set src instantly if IntersectionObserve doesn't exist

--- src/components/LazyImg/index.js
+++ src/components/LazyImg/index.js
@@ -1,41 +1,51 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const targets = new Map();
 
-const io = new IntersectionObserver(entries => {
-  entries.forEach(({ isIntersecting, target }) => {
-    if (isIntersecting) {
-      targets.get(target).forEach(async ({ src, img }) => {
-        img.setAttribute('src', (await src()).default);
-      });
-
-      requestIdleCallback(() => {
-        io.unobserve(target);
-        targets.delete(target);
-      });
-    }
-  })
-});
+let io;
+if (window.IntersectionObserver) {
+  io = new IntersectionObserver(entries => {
+    entries.forEach(({ isIntersecting, target }) => {
+      if (isIntersecting) {
+        targets.get(target).forEach(async ({ src, setSrc }) => {
+          setSrc((await src()).default);
+        });
+  
+        requestIdleCallback(() => {
+          io.unobserve(target);
+          targets.delete(target);
+        });
+      }
+    })
+  });
+}
 
 const LazyImg = ({ src, alt, target, ...props }) => {
+  const [currentSrc, setSrc] = useState('data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=');
   const imgRef = useRef();
 
   useEffect(() => {
-    const observeTarget = target
-      ? document.querySelector(target)
-      : imgRef.current;
+    if (window.IntersectionObserver) {
+      const observeTarget = target
+        ? document.querySelector(target)
+        : imgRef.current;
 
-    targets.set(observeTarget, (targets.get(observeTarget) || []).concat({
-      src,
-      img: imgRef.current
-    }));
-    io.observe(observeTarget);
-  }, [imgRef, src, target]);
+      targets.set(observeTarget, (targets.get(observeTarget) || []).concat({
+        src,
+        setSrc
+      }));
+      io.observe(observeTarget);
+
+      return () => io.unobserve(observeTarget);
+    } else {
+      src().then(module => setSrc(module.default));
+    }
+  }, [src, target]);
 
   return (
     <img
-      src="data:image/png;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+      src={currentSrc}
       alt={alt}
       ref={imgRef}
       {...props}
