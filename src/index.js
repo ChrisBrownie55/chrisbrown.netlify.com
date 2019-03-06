ca04d41e71c276111306eebb4738cfae75a1b925
Christopher Brown <chbphone55@gmail.com>
2019-03-05 22:23:55 -0700

    remove console logs

--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-console.log('requestIdleCallback not loaded');
-
 async function loadRequestIdleCallback() {
   if (!window.requestIdleCallback) {
     const polyfill = await import('./polyfills/requestIdleCallback');
     window.requestIdleCallback = polyfill.requestIdleCallback;
     window.cancelIdleCallback = polyfill.cancelIdleCallback;
-    console.log(window.requestIdleCallback);
   }
 }
 
 Promise.all([loadRequestIdleCallback()])
   .then(() => {
-    console.log('requestIdleCallback loaded');
     ReactDOM.render((
       <App />
     ), document.getElementById('root'));
